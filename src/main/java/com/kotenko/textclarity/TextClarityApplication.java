6dbfd48b18c0daca50104e8f00427055b78f0dff
Alex <akotenko@prezna.com>
2023-10-02 08:15:13 -0400

    0210L: Innitial commit

---
+++ src/main/java/com/kotenko/textclarity/TextClarityApplication.java
@@ -0,0 +1,13 @@
+package com.kotenko.textclarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextClarityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextClarityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kotenko/textclarity/TextClarityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotenko.textclarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextClarityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
